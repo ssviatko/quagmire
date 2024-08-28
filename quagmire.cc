866f7c052ed35bd16eec61d68f2f2aa11484df6d
Stephen Sviatko <ssviatko@gmail.com>
2024-08-27 19:47:58 -0700

    Help system added

--- quagmire.cc
+++ quagmire.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <optional>
 
 #include <unistd.h>
 
@@ -6,15 +8,22 @@
 
 void usage()
 {
-	std::cout << "Quagmire" << std::endl << std::endl;
-	std::cout << "quagmire -c file.quagmire \"passphrase 1\" file1 \"passphrase2\" file2 ..." << std::endl;
-	std::cout << "  Creates new quagmire containing file1, file2, etc. using the specified passphrases" << std::endl;
-	std::cout << "  for each file." << std::endl;
-	std::cout << "  Options: -r <ratio> will specify the ratio of encrypted data to random data," << std::endl;
-	std::cout << "  default is 30." << std::endl;
-	std::cout << "quagmire -c file.quagmire -r 40 \"passphrase 1\" file1 \"passphrase2\" file2 ..." << std::endl;
-	std::cout << "  In this example, the ratio is set to 40. This means that 40% of the file will" << std::endl;
-	std::cout << "  contain file data, while 60% of it will contain random data." << std::endl;
+	std::cout << "Quagmire v" << RELEASE_NUMBER << " build " << BUILD_NUMBER << std::endl;
+	std::cout << "Built on: " << BUILD_DATE << std::endl << std::endl;
+	ss::data l_help;
+	try {
+		l_help.load_file("help.txt");
+		std::optional<std::string> line;
+		do {
+			line = l_help.read_std_str_delim();
+			if (line.has_value())
+				std::cout << line.value() << std::endl;
+		} while (line.has_value());
+	} catch (ss::data_exception& e) {
+		std::cout << "unable to display help file." << std::endl;
+		std::cout << "reason: " << e.what() << std::endl;
+		exit(EXIT_FAILURE);
+	}
 	exit(EXIT_SUCCESS);
 }
 
