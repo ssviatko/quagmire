3c02b1198d5d7731a054c72ddb1aaec749fc6197
Stephen Sviatko <ssviatko@gmail.com>
2024-08-27 22:01:31 -0700

    Policing of structure size and data lengths

--- qm-engine.cc
+++ qm-engine.cc
@@ -4,7 +4,11 @@ namespace qm {
 
 void quagmire_create(bool a_debug, bool a_verbose, const std::string& a_file, unsigned int a_ratio, std::deque<std::string>& a_args)
 {
-	
+	if (a_debug) {
+		std::cout << "sizeof sector: " << sizeof(sector_t) << std::endl;
+		std::cout << "data_size: " << data_size << std::endl;
+		std::cout << "pt_data_size: " << pt_data_size << std::endl;
+	}
 }
 
 void quagmire_extract(bool a_debug, bool a_verbose, const std::string& a_file, std::deque<std::string>& a_args)
--- qm-engine.h
+++ qm-engine.h
@@ -13,20 +13,25 @@
 
 namespace qm {
 
+constexpr std::uint16_t data_size = 8106;
+
+// BF7-CBC-HMAC-SHA2-256 encrypted buffer will contain exactly 32 bytes less than the actual plaintext data
+// because of the 32 byte HMAC which sits at the beginning of the buffer.
+
+constexpr std::uint16_t pt_data_size = data_size - 32;
+
 // 8192 byte sector
-typedef struct {
-	// 64 bytes
+typedef struct __attribute__((__packed__)) {
+	std::uint32_t crc;
 	std::uint8_t fileindex;
 	std::uint8_t namelen;
 	std::uint8_t name[62];
-	// 18 bytes
 	std::uint32_t chunk_id;
 	std::uint16_t chunk_len;
 	std::uint32_t total_chunks;
 	std::uint32_t prev_sector;
 	std::uint32_t next_sector;
-	// 8112 byte data buffer
-	std::uint8_t data[8110];
+	std::uint8_t data[data_size];
 } sector_t;
 
 void quagmire_create(bool a_debug, bool a_verbose, const std::string& a_file, unsigned int a_ratio, std::deque<std::string>& a_args);
