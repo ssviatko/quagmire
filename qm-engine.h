7fd6112692c272c7d418d65d48175d754f52a574
Stephen Sviatko <ssviatko@gmail.com>
2024-08-27 21:31:25 -0700

    added length field to sector definition

--- qm-engine.h
+++ qm-engine.h
@@ -19,13 +19,14 @@ typedef struct {
 	std::uint8_t fileindex;
 	std::uint8_t namelen;
 	std::uint8_t name[62];
-	// 16 bytes
+	// 18 bytes
 	std::uint32_t chunk_id;
+	std::uint16_t chunk_len;
 	std::uint32_t total_chunks;
 	std::uint32_t prev_sector;
 	std::uint32_t next_sector;
 	// 8112 byte data buffer
-	std::uint8_t data[8112];
+	std::uint8_t data[8110];
 } sector_t;
 
 void quagmire_create(bool a_debug, bool a_verbose, const std::string& a_file, unsigned int a_ratio, std::deque<std::string>& a_args);
